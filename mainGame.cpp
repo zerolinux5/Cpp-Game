f0053d6105fcd347ec02d2427437628e4509e735
Jesus Magana <maganazero5@gmail.com>
2014-09-29 13:50:53 -0700

    Fixed map class

--- mainGame.cpp
+++ mainGame.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-
-using namespace std;
-
 #include "Player.cpp"
 #include "Monster.cpp"
 #include "Map.cpp"
+
+using namespace std;
 
 void battle(Player p1, Monster m1){
 	int playerAtk = p1.getAtk() - m1.getDef();
@@ -17,22 +16,13 @@ void battle(Player p1, Monster m1){
 	cout << "Monster Hp left:" << m1.getHp() << endl;
 }
 
-void mapToField(Map inMap){
-	cout << inMap.getMon1() << endl;
-	cout << inMap.getMon2() << endl;
-	cout << inMap.getNeutral() << endl;
-	cout << inMap.getPlayer2() << endl;
-	cout << inMap.getPlayer1() << endl;
-	
-}
-
 int main()
 {
 	Player p1;
 	Monster m1;
 	Map beginner;
 
-	mapToField(beginner);
+	beginner.printField();
 
 	return 0;
 }
--- map.cpp
+++ map.cpp
@@ -1,39 +1,37 @@
 #include <iostream>
 using namespace std;
+const int SIZE = 5;
 
 class Map{
-	string mon1;
-	string mon2;
-	string neutral;
-	string player2;
-	string player1;
 	int difficulty;
+	string field[SIZE][SIZE];
 
    public:
-	Map(string, string, string, string, string, int);
 	Map();
-	string getMon1() { return mon1;}
-	string getMon2() {return mon2;}
-	string getNeutral() {return neutral;}
-	string getPlayer2() {return player2;}
-	string getPlayer1() {return player1;}
+	void printField();
 	int getDifficulty() {return difficulty;}
 };
 
-Map::Map(string inM1, string inM2, string inN, string inP2, string inP1, int inD){
-	mon1 = inM1;
-	mon2 = inM2;
-	neutral = inN;
-	player2 = inP2;
-	player1 = inP1;
-	difficulty = inD;
-}
 
 Map::Map(){
-	mon1 = "*****";
-	mon2 = "     ";
-	neutral = "     ";
-	player2 = "     ";
-	player1 = "+++++";
-	difficulty = 1;
+	for(int i = 0; i < SIZE; i++){
+		for(int j = 0; j < SIZE; j++){
+			if(i == 0){
+				field[i][j] = '*';
+			} else if (i == 4){
+				field[i][j] = '+';
+			} else {
+				field[i][j] = ' ';
+			}
+		}
+	}
+}
+
+void Map::printField(){
+	for(int i = 0; i < SIZE; i++){
+		for(int j = 0; j < SIZE; j++){
+			cout << field[i][j];
+		}
+		cout << endl;
+	}
 }
