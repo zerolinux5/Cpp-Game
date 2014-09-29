9a4c6f188668ccb1d215fc5caae951d160c91c10
Jesus Magana <maganazero5@gmail.com>
2014-09-29 12:39:50 -0700

    Made monster class

---
+++ monster.cpp
@@ -0,0 +1,23 @@
+class Monster {
+	int hp;
+	int atk;
+	int def;
+   public:
+	Monster(int, int, int);
+	Monster();
+	int getHp(){ return hp;}
+	int getAtk(){ return atk;}
+	int getDef(){ return def;}
+};
+
+Monster::Monster(int inHp, int inAtk, int inDef){
+	hp = inHp;
+	atk = inAtk;
+	def = inDef;
+}
+
+Monster::Monster(){
+	hp = 50;
+	atk = 1;
+	def = 1;
+}
