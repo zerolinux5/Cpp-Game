aca83c6c02df55872b00475d9b33231bdc093a67
Jesus Magana <maganazero5@gmail.com>
2014-11-06 16:22:18 -0800

    Works but style is wrong

---
+++ mainGame.cpp
@@ -0,0 +1,10 @@
+#include "monster.cpp"
+
+int main()
+{
+	//Monster stats HP, SP, Atk, Def, Element
+	Monster m1 (10,8, 5, 3, "Darkness");
+	m1.printStats();
+
+	return 0;
+}
--- monster.cpp
+++ monster.cpp
@@ -1,23 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 class Monster{
 	int hp;
 	int sp;
-	int def;
 	int atk;
+	int def;
 	string element;
    public:
-	void Monster(int, int, int, int, string);
+	Monster(int, int, int, int, string);
 	int getHp() {return hp;}
 	int getSp() {return sp;}
 	int getDef() {return def;}
 	int getAtk() {return atk;}
 	string getElement() {return element;}	
+	void printStats();
 };
 
-Monster::Monster(int newHp, int newSp, int newDef, int newAtk, string newElement)
+Monster::Monster(int newHp, int newSp, int newAtk, int newDef, string newElement)
 {
-	hp = newHo;
+	hp = newHp;
 	sp = newSp;
 	atk = newAtk;
 	def = newDef;
 	element = newElement;
+}
+
+void Monster::printStats(){
+	cout << "Hp:" << hp << endl;
+	cout << "Sp:" << sp << endl;
+	cout << "Atk:" << atk << endl;
+	cout << "Def:" << def << endl;
+	cout << "Element:" << element << endl;
 }
