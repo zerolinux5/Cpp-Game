6a7b0004ffa560c071007140bbd05e29da54baeb
Jesus Magana <maganazero5@gmail.com>
2014-11-08 17:59:36 -0800

    Have basic funcions down

--- stage.cpp
+++ stage.cpp
@@ -1,1 +1,24 @@
-placeholder
+#include <iostream>
+#include <string>
+#include "stage.h"
+
+Stage::Stage(std::string newName, std::string newDescription)
+{
+	name = newName;
+	description = newDescription;
+}
+
+void Stage::printStage()
+{
+	int x = 0, y = 0;
+
+	for(; y < BOARDSIZE; y++){
+		for(;x < BOARDSIZE; x++){
+			if (x == BOARDSIZE - 1){
+				std::cout << board[y][x] << std::endl;
+			} else {
+				std::cout << board[y][x] << " ";
+			}
+		}
+	}
+}
--- stage.h
+++ stage.h
@@ -6,12 +6,13 @@ const int BOARDSIZE = 10;
 class Stage{
 	std::string name;
 	std::string description;
-	string board[BOARDSIZE][BOARDSIZE];
+	std::string board[BOARDSIZE][BOARDSIZE];
    public:
-   	Stage(string, string, int);
+   	Stage(std::string, std::string);
    	std::string getName() {return name;}
-   	std::string getDescription {return description;}
+   	std::string getDescription() {return description;}
    	std::string getBoardLocation(int x, int y) {return board[x][y];}
+   	void printStage();
 };
 
 #endif
