51857e1ab70049718e002c3b84865adf4b9ba0b2
Jesus Magana <maganazero5@gmail.com>
2014-12-06 22:38:50 -0800

    User chooses stage

--- game.cpp
+++ game.cpp
@@ -3,14 +3,28 @@
 
 #include "stage.h"
 
+#define MAXSTAGES 3
+
 using namespace std;
 
 int main()
 {
-	int player, enemy;
+	int stage, player, enemy;
 	Stage s1 ("Grassland", "Grass stage with trees", 1);
 	Stage s2 ("Marshes", "Muddy land with swamps", 2);
 	Stage s3 ("Mountains", "Mountain area with high peaks", 3);
+
+	do{
+		s1.printStageName();
+		cout << endl;
+		s2.printStageName();
+		cout << endl;
+		s3.printStageName();
+		cout << endl;
+
+		cout << "Enter which Stage:(1-3):";
+		cin >> stage;
+	} while (stage < 0 || stage > MAXSTAGES);
 
 	return 0;
 }
